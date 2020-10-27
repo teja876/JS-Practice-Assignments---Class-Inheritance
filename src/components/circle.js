34370a46e2f4ddf1259cc84a3895a31242041f90
teja876 <teja.katakam123@gmail.com>
2020-10-27 17:07:45 +0530

    update code

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,17 @@
 
 // declare class
 
-
 // export class using module.exports
+
+const { default: Shape } = require("./shape");
+
+// declare class
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea() {}
+}
+
+// export
+export default Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
 
 // export class using module.exports
+
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape() {}
+  calculateArea() {}
+}
+
+// export class
+export default Shape;
